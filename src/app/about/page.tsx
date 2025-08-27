330f938aae92a9a4ffc648cb22d2b1e360a3704e
cuddleminddev <cuddleminddev@gmail.com>
2025-08-27 12:59:42 +0530

    mission section in about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import HomeMission from "../home/mission/page";
 import Welcome from "./welcome/page";
 
 export default function AboutPage() {
     return (
         <>
         <Welcome/>
+        <HomeMission/>
         </>
     )
 }
