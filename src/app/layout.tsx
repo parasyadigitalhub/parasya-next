61cc8487578876e270eec6408ebc2ef0f2dfd4ec
cuddleminddev <cuddleminddev@gmail.com>
2025-09-09 13:36:44 +0530

    meta test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,32 @@ import ClientLayout from "./ClientLayout";
 export const metadata = {
   title: "Parasya",
   description: "Elevating Your Identity",
+  keywords: "branding, design, marketing, Parasya",
+  authors: [{ name: "Parasya" }],
+  viewport: "width=device-width, initial-scale=1",
+  openGraph: {
+    title: "Parasya - Elevating Your Identity",
+    description: "Parasya helps elevate your brand identity through innovative solutions.",
+    url: "https://parasya.in",
+    siteName: "Parasya",
+    type: "website",
+    images: [
+      {
+        url: "/parasya/parasya.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Parasya Branding"
+      }
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Parasya - Elevating Your Identity",
+    description: "Parasya helps elevate your brand identity through innovative solutions.",
+    images: ["/images/twitter-image.png"],
+    site: "@Parasya",
+    creator: "@Parasya",
+  },
 };
 
 export default function RootLayout({
